7e36a2e745048e14cf30ff64ce6cd7b180678269
anthony-murè <tony.mure99@gmail.com>
2025-08-29 19:00:06 +0200

    [add]Milestone-2

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
 
 const articlesList = ['skateboard', 'bicicletta', 'monopattino', 'driftskates', 'monociclo']
 
 const ArticlesList = () => {
+
+  const [articles, setArticles] = useState(articlesList);
+  const [newArticle, setNewArticle] = useState('');
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    const article = newArticle.trim();
+    const copyArticles = [...articles, article];
+    setArticles(copyArticles);
+  }
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-12">
           <ul>
-            {articlesList.map((article, index) => (
-            <li key={index}>{article}</li>
+            {articles.map((article, index) => (
+              <li key={index}>{article}</li>
            ))};
           </ul>
+        </div>
+        <div className="col-12">
+          <form onSubmit={ handleForm }>
+            <input type="text" value={newArticle} onChange={(event) => {
+              setNewArticle(event.target.value);
+            }}
+            />
+             <button className="btn btn-success ms-3">Aggiungi</button>
+          </form>
         </div>
       </div>
       
